ebfaefae6baf92f20f603a36b358a8e9e9eaa31a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-04-15 13:46:18 +0900

    Finish lesson 258

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,18 @@
+import CoreData
+import UIKit
+
+class CategoryViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - TableView Datasource Methods
+
+    // MARK: - Data Manipulation Methods
+
+    // MARK: - Add New Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {}
+
+    // MARK: - TableView Delegate Methods
+}
