08a1670315090a765d7d8c03fe733432e4251968
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2025-04-24 16:29:06 +0900

    Finish lesson 267

--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -4,5 +4,6 @@ import RealmSwift
 class Item: Object {
     @objc dynamic var title: String = ""
     @objc dynamic var done: Bool = false
+    @objc dynamic var dateCreated: Date?
     var parentCategory = LinkingObjects<Category>(fromType: Category.self, property: "items")
 }
